d1f665dc233b88545a3d7049f54e5aa189fa4b9c
NishantKr97 <nishantkr1997@gmail.com>
2018-04-02 15:56:49 +0530

    Server Side

--- SocketChatApp/Server_Side/MainActivity.java
+++ SocketChatApp/Server_Side/MainActivity.java
@@ -1,48 +1,33 @@
-package com.androidsrc.client;
+package com.androidsrc.server;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	TextView response;
-	EditText editTextAddress, editTextPort;
-	Button buttonConnect, buttonClear;
+	Server server;
+	TextView infoip, msg;
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		infoip = (TextView) findViewById(R.id.infoip);
+		msg = (TextView) findViewById(R.id.msg);
+		server = new Server(this);
+		infoip.setText(server.getIpAddress()+":"+server.getPort());
 
-		editTextAddress = (EditText) findViewById(R.id.addressEditText);
-		editTextPort = (EditText) findViewById(R.id.portEditText);
-		buttonConnect = (Button) findViewById(R.id.connectButton);
-		buttonClear = (Button) findViewById(R.id.clearButton);
-		response = (TextView) findViewById(R.id.responseTextView);
-
-		buttonConnect.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				Client myClient = new Client(editTextAddress.getText()
-						.toString(), Integer.parseInt(editTextPort
-						.getText().toString()), response);
-				myClient.execute();
-			}
-		});
-
-		buttonClear.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				response.setText("");
-			}
-		});
+		
 	}
 
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		server.onDestroy();
+	}
+
+	
 }
