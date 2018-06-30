b286f982c3d58ae65f6175c3c57fd3e22116e233
NishantKr97 <nishantkr1997@gmail.com>
2018-06-30 16:02:42 +0530

    Downloading Part Remaining

---
+++ DownloadManager/app/src/main/java/com/example/nishant/downloadmanager/Country.java
@@ -0,0 +1,22 @@
+/**
+ * Created by NISHANT on 30-06-2018.
+ */
+package com.example.nishant.downloadmanager;
+public class Country {
+    String code = null;
+    String name = null;
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
